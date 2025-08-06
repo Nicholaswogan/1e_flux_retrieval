73538e413419dd4daf36a8e10638d5035f0fa51d
Nick Wogan <nicholaswogan@gmail.com>
2025-08-06 08:35:52 -0700

    moving to new system

--- climate_grid.py
+++ climate_grid.py
@@ -1,66 +1,21 @@
 import warnings
 warnings.filterwarnings('ignore')
 
-from clima import ClimaException
-import pickle
-import multiprocessing as mp
-from tqdm import tqdm
-import os
+from gridutils import make_grid
 import numpy as np
 from threadpoolctl import threadpool_limits
 _ = threadpool_limits(limits=1)
 
-import grid_utils
 import utils
-
-# This stuff has to be top level but does not have to change
-
-def worker(i, x, q):
-    res = model(x)
-    q.put((i,x,res,))
-
-def main(gridvals, filename, ncores):
-
-    inputs = grid_utils.get_inputs(gridvals) # get inputs
-
-    # Exception if the file already exists
-    if os.path.isfile(filename):
-        raise Exception(filename+' already exists!')
-    else:
-        if not os.path.isfile(filename):
-            with open(filename, 'wb') as f:
-                pass
-
-    manager = mp.Manager()
-    q = manager.Queue()
-    with mp.Pool(ncores+1) as pool:
-
-        # Put listener to work first
-        watcher = pool.apply_async(grid_utils.listener, (q,filename,))
-
-        # Fire off workers
-        jobs = []
-        for i in range(inputs.shape[0]):
-            x = inputs[i,:]
-            job = pool.apply_async(worker, (i, x, q))
-            jobs.append(job)
-
-        # Collect results from the workers through the pool result queue
-        for job in tqdm(jobs): 
-            job.get()
-
-        # Kill the listener
-        q.put('kill')
-
-# Make the model function
 
 # Initialize the climate model
 CLIMATE_MODEL = utils.AdiabatClimateRobust(
     'inputs/species_climate.yaml',
     'inputs/settings_climate.yaml',
-    'inputs/TRAPPIST1e_stellar_flux.txt'
+    'inputs/TRAPPIST1e_hazmat.txt'
 )
 CLIMATE_MODEL.verbose = False
+CLIMATE_MODEL.P_top = 1e2
 
 def model(x):
     c = CLIMATE_MODEL
@@ -98,7 +53,7 @@ def make_result(x, c, converged):
 
     # Save results as 32 bit floats
     result = {}
-    result['converged'] = converged
+    result['converged'] = np.array(converged)
     result['x'] = x.astype(np.float32)
     result['P'] = P.astype(np.float32)
     result['z'] = z.astype(np.float32)
@@ -111,86 +66,19 @@ def make_result(x, c, converged):
 def get_gridvals():
     log10PN2 = np.array([-1.0, 0.0])
     log10PCO2 = np.arange(-5,1.01,1)
-    log10PO2 = np.arange(-7,1.01,2)
-    log10PCO = np.arange(-7,1.01,2)
-    log10PH2 = np.arange(-6,0.01,2)
+    log10PO2 = np.arange(-7,1.01,1)
+    log10PCO = np.arange(-7,1.01,1)
+    log10PH2 = np.arange(-6,0.01,1)
     log10PCH4 = np.arange(-7,1.01,1)
     gridvals = (log10PN2,log10PCO2,log10PO2,log10PCO,log10PH2,log10PCH4)
     return gridvals
 
-def fix_climate_grid(filename, outfile):
-
-    c = CLIMATE_MODEL
-
-    res = []
-    i = 0
-    with open(filename,'rb') as f:
-        while True:
-            try:
-                tmp = pickle.load(f)
-                res.append(tmp)
-            except EOFError:
-                break
-
-    # Find failed runs 
-    failed = []
-    for i in range(len(res)):
-        val = res[i]
-        if not val[2]['converged']:
-            failed.append(val)
-
-    # Fix the failed runs
-    fixed = []
-    for i,val in enumerate(failed):
-        print('Working on failed %i'%i)
-        for perturb in [-1,1,-2,2,-3,3]:
-            converged = False
-            for j in range(len(res)):
-                if res[j][0] == val[0]+perturb and res[j][2]['converged']:
-                    # Run model that worked
-                    model(res[j][1])
-
-                    # Now run model that didn't work with initial conditions
-                    # from nearby one that did work. 
-                    P_i = x_to_Pi(val[1], c)
-                    try:
-                        converged = c.RCE(P_i, c.T_surf, c.T, c.convecting_with_below)
-                    except ClimaException:
-                        converged = False
-                    break
-            if converged:
-                break
-            
-        result = make_result(val[1], c, converged)
-        fixed.append((val[0],val[1],result))
-        print(converged)
-
-    res_new = []
-    for val in res:
-        if val[2]['converged']:
-            res_new.append(val)
-        else:
-            found = False
-            for val1 in fixed:
-                if val1[0] == val[0]:
-                    found = True
-                    replace = val1
-            
-            if not found:
-                raise Exception()
-
-            res_new.append(replace)
-
-    with open(outfile, 'wb') as f:
-        pass
-
-    for val in res_new:
-        with open(outfile, 'ab') as f:
-            pickle.dump(val,f)
-
 if __name__ == "__main__":
-    filename = 'results/climate_v1.pkl' # Specify output filename
-    ncores = 4 # Specify number of cores
-    gridvals = get_gridvals()
-    main(gridvals, filename, ncores)
-    fix_climate_grid(filename, 'results/climate_fixed_v1.pkl')
+    print(np.prod([len(x) for x in get_gridvals()]))
+    # mpiexec -n X python climate_grid.py
+    # make_grid(
+    #     model_func=model, 
+    #     gridvals=get_gridvals(), 
+    #     filename='results/climate_v1.h5', 
+    #     progress_filename='results/climate_v1.log'
+    # )
--- input_files.py
+++ input_files.py
@@ -2,29 +2,47 @@
 import os
 from photochem.utils import stars
 from photochem.utils import zahnle_rx_and_thermo_files
+from photochem.utils import climate
 import requests
 import tempfile
 import numpy as np
 from astropy.io import fits
+from astropy import constants
+import planets
+
+def create_climate_inputs():
+    # Species file
+    climate.species_file_for_climate(
+        filename='inputs/species_climate.yaml', 
+        species=['H2O','CO2','N2','H2','CH4','CO','O2'], 
+        condensates=['H2O','CO2','CH4']
+    )
+
+    # Settings file
+    climate.settings_file_for_climate(
+        filename='inputs/settings_climate.yaml', 
+        planet_mass=float(planets.TRAPPIST1e.mass*constants.M_earth.cgs.value), 
+        planet_radius=float(planets.TRAPPIST1e.radius*constants.R_earth.cgs.value), 
+        surface_albedo=0.1, 
+        number_of_layers=50, 
+        number_of_zenith_angles=4, 
+        photon_scale_factor=1.0
+    )
 
 def create_zahnle_HNOC():
-    "Creates a reactions file with H, N, O, C species."
-    if not os.path.isfile('inputs/zahnle_HNOC.yaml'):
-        zahnle_rx_and_thermo_files(
-            atoms_names=['H', 'N', 'O', 'C'],
-            rxns_filename='inputs/zahnle_HNOC.yaml',
-            thermo_filename=None
-        )
+    zahnle_rx_and_thermo_files(
+        atoms_names=['H', 'N', 'O', 'C'],
+        rxns_filename='inputs/zahnle_HNOC.yaml',
+        thermo_filename=None
+    )
 
 def create_TRAPPIST1e_stellar_flux():
-    "Creates TRAPPIST-1 spectrum at planet e"
-    if not os.path.isfile('inputs/TRAPPIST1e_stellar_flux.txt'):
-        _ = stars.hazmat_spectrum(
-            star_name='TRAPPIST-1',
-            model='1a',
-            outputfile='inputs/TRAPPIST1e_stellar_flux.txt',
-            stellar_flux=0.646*1361.0, # W/m^2 (Agol et al. 2021)
-        )
+    _ = stars.hazmat_spectrum(
+        star_name='TRAPPIST-1',
+        model='1a',
+        outputfile='inputs/TRAPPIST1e_hazmat.txt',
+        stellar_flux=planets.TRAPPIST1e.stellar_flux,
+    )
 
 def create_TRAPPIST1_flux_picaso():
 
@@ -54,6 +72,7 @@ def main():
     create_zahnle_HNOC()
     create_TRAPPIST1e_stellar_flux()
     create_TRAPPIST1_flux_picaso()
+    create_climate_inputs()
 
 if __name__ == '__main__':
     main()
